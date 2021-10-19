7611ea63fe5b2a0ec227579e1f107e17db68d866
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-10-19 07:56:53 -0700

    async function instead of callback function
    Checks `stream.writable` instead of `stream.socket.writable`
    
    Removed Makefile because I don't think it's needed
    
    Changed tests to use baretest because tape was not working and node.js assert module already has assertion methods.
    
    Added `pnpm-lock.yaml` to gitignore because I did `pnpm i`

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-module.exports = function write(stream, chunk, cb){
-  var errored = false;
+const { once } = require('events')
 
-  stream.once('error', error);
-  function error(err){
-    errored = true;
-    cb(err);
-  }
-
-  if (stream.socket && !stream.socket.writable) {
-    var err = new Error('write after end');
+module.exports = async function write(stream, chunk){
+  if (!stream.writable) {
+    var err = new Error('Stream is not writable');
     err.status = 200;
-    setImmediate(cb.bind(null, err));
-    return;
+    throw err
   }
 
-  if (stream.write(chunk)) {
-    stream.removeListener('error', error);
-    if (errored) return;
-    setImmediate(cb.bind(null, null, stream.writable));
-  } else {
-    stream.once('drain', next);
-    stream.once('finish', next);
-
-    function next(){
-      stream.removeListener('error', error);
-      stream.removeListener('drain', next);
-      stream.removeListener('finish', next);
-      cb(null, stream.writable);
-    }
+  if (!stream.write(chunk)) {
+    await Promise.race([once(stream, 'drain'), once(stream, 'finish')])
   }
+  return stream.writable
 };
 
