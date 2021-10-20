072eaa61462abe2790fc94b77657ccba8fe2e7ee
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-10-20 12:24:15 -0700

    No special `err.code` when stream isn't writable

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const { once } = require('events')
 
 module.exports = async function write(stream, chunk){
-  if (!stream.writable) {
-    var err = new Error('Stream is not writable');
-    err.status = 200;
-    throw err
-  }
+  if (!stream.writable) throw new Error('Stream is not writable');
 
   if (!stream.write(chunk)) {
     await Promise.race([once(stream, 'drain'), once(stream, 'finish')])
