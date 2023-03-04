a0e0baaa15f48ad09c0988000df161719acdc215
Julian Gruber <julian@juliangruber.com>
2023-03-04 14:34:53 +0100

    prevent listener leak through abort controller. #6

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ export default async function write (stream, chunk) {
   assert(stream.writable, 'Stream is not writable')
 
   if (!stream.write(chunk)) {
-    await Promise.race([once(stream, 'drain'), once(stream, 'finish')])
+    const controller = new AbortController()
+    await Promise.race([
+      once(stream, 'drain', controller.signal),
+      once(stream, 'finish', controller.signal)
+    ])
+    controller.abort()
   }
   return stream.writable
 }
