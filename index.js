20b93747663457425c755068432aa91b9c7fb950
Julian Gruber <julian@juliangruber.com>
2023-03-04 14:32:16 +0100

    refactor

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { once } from 'node:events'
+import assert from 'node:assert'
 
 export default async function write (stream, chunk) {
-  if (!stream.writable) throw new Error('Stream is not writable')
+  assert(stream.writable, 'Stream is not writable')
 
   if (!stream.write(chunk)) {
     await Promise.race([once(stream, 'drain'), once(stream, 'finish')])
   }
   return stream.writable
-};
+}
